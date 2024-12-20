4951629c50ae029f4a6de51b4a006954c4af3ba8
THELoer <krasnod1203@mail.ru>
2024-12-20 20:17:36 +0500

    added pwd

---
+++ pwd/target/debug/deps/pwd-b86f21270c7e7789.d
@@ -0,0 +1,5 @@
+/home/goydauser/gits/rust-learnnn/simple-rust-cli-utils/pwd/target/debug/deps/pwd-b86f21270c7e7789: src/main.rs
+
+/home/goydauser/gits/rust-learnnn/simple-rust-cli-utils/pwd/target/debug/deps/pwd-b86f21270c7e7789.d: src/main.rs
+
+src/main.rs:
---
+++ pwd/target/debug/pwd.d
@@ -0,0 +1,1 @@
+/home/goydauser/gits/rust-learnnn/simple-rust-cli-utils/pwd/target/debug/pwd: /home/goydauser/gits/rust-learnnn/simple-rust-cli-utils/pwd/src/main.rs
